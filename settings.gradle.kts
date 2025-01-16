a19d1ec92c89f680af47b65aa7ceaa979ab4b72e
Muratcan Yeldan <muratcan_yeldan@hotmail.com>
2025-01-16 12:01:43 +0300

    kotlin version update

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,18 +14,6 @@ dependencyResolutionManagement {
     }
 }
 
-// Add version catalogs for plugins
-@Suppress("UnstableApiUsage")
-dependencyResolutionManagement {
-    versionCatalogs {
-        create("libs") {
-            plugin("firebase-crashlytics", "com.google.firebase.crashlytics").version("2.9.9")
-            plugin("gms", "com.google.gms.google-services").version("4.4.2")
-            plugin("ksp", "com.google.devtools.ksp").version("1.9.22-1.0.17")
-        }
-    }
-}
-
 rootProject.name = "PetVaccineTracker"
 include(":app")
  
